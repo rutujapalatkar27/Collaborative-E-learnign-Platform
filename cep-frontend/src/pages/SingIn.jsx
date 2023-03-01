254fdb04e538b8dc0f6c139a04a3dedf5977cb30
SushanPoojary <sushanpoojary@gmail.com>
2023-02-28 18:12:37 -0800

    Autofocus and localstorage

--- cep-frontend/src/pages/SingIn.jsx
+++ cep-frontend/src/pages/SingIn.jsx
@@ -29,7 +29,7 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
-  let navigate = useNavigate();
+//   let navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
